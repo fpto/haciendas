8f0c4f8d77916d7f63a045da9ccf109b1ebb6e7d
fpto <fabricio.puerto@outlook.com>
2017-12-15 22:20:09 -0600

    Fixed the user permissios

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -1,8 +1,8 @@
 class PlotsController < ApplicationController
-  before_action :require_user, only: [:new, :create, :edit, :destroy]
-  before_action :require_editor, only: [:new, :create, :edit]
-  before_action :require_admin, only: [:new, :create, :edit, :destroy]
-  
+  before_action :require_user, only: [:new, :create, :edit, :destroy, :update]
+  before_action :require_editor, only: [:new, :create, :edit, :update]
+  before_action :require_admin, only: [:destroy]
+
   before_action :set_plot, only: [:show, :edit, :update, :destroy]
 
   # GET /plots
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
   def editor?
-    self.role == 'editor'
+    self.role == 'editor' or 'admin'
   end
-  
+
   def admin?
     self.role == 'admin'
   end
