1e1eeeffe7486d569b5d369dbdaffbb383057fa2
fpto <fabricio.puerto@outlook.com>
2017-12-29 21:42:10 -0600

    New UI layout and configure animals and weights kpis

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,41 +1,10 @@
 class PagesController < ApplicationController
   def home
-    @weights = Weight.all
-    #Every animal should have a weight for data to be accurate
-    @animals = Animal.all
-    @number_of_animals = Animal.count()
-
-    # Calculate the average Max weight:
-    @max_weights =  Weight.group('animal_id').maximum(:weight)
-    @sum_max_weights = 0
-    @max_weights.each { |key, value| @sum_max_weights += value }
-    @average_max_weight = @sum_max_weights/@number_of_animals
-
-    # Calculates average date dif - Días desde ingreso
-    @min_dates = Weight.group('animal_id').minimum(:date)
-    @sum_date_dif = 0
-    @min_dates.each {|key,value| @sum_date_dif += (Date.today - value.to_date).to_f }
-    @average_date_dif = @sum_date_dif/@number_of_animals
-
-    @min_weights = Weight.group('animal_id').minimum(:weight)
-    @weight_gains = @max_weights.merge(@min_weights) { |k, v1, v2| v1 - v2 }
-    # It's working until here, next an hash with date-difs
-    @max_dates = Weight.group('animal_id').maximum(:date)
-    @date_difs = {}
-    @date_difs = @max_dates.merge(@min_dates) { |k, v1, v2| (v1 - v2).to_i }
-    #Now we need to divide each element of  weight_gains with date_difs
-    @daily_weight_gains = @weight_gains.merge(@date_difs) { |k, v1, v2| if v2 != 0 then v1 / v2 else 0 end }
-    #Finally we sum them up and then divide them by the number of animals
-    @sum_daily_weight_gains = 0
-    @daily_weight_gains.each {|key,value| @sum_daily_weight_gains += value}
-    @average_daily_gain = @sum_daily_weight_gains /@number_of_animals
-    # This calculates the average daily gain during stay
-    # I use in the home page now the latest daily gain, since it's more dynamic
-
     # This is to calculate the latest two weights of each animal
     @latest_weights = Weight.select(
       "dates.animal_id as animal_id,
       animals.ranch as ranch,
+      animals.species as species,
     weights.weight as last_weight,
     dates.latest_date - dates.before_date as days_between_weights,
     date(NOW()) - dates.latest_date as days_since_last_weight,
@@ -57,6 +26,7 @@ class PagesController < ApplicationController
       GROUP BY animal_id) as dates ON weights.animal_id = dates.animal_id AND weights.date = dates.latest_date
       JOIN weights w2 ON  w2.animal_id = dates.animal_id AND w2.date = dates.before_date
       JOIN animals ON animals.id = dates.animal_id").order("animal_id")
+    # General Average Daily Gain
     @sum_latest_daily_gains = 0
     @animals_with_gain = 0
     @latest_weights.each do |animal|
@@ -67,12 +37,83 @@ class PagesController < ApplicationController
       end
     end
     @latest_average_daily_gain = @sum_latest_daily_gains / @animals_with_gain
+    # Bovine and Ovine Average Daily Gain and Average Weight
+    @sum_latest_daily_gains_b =  @animals_with_gain_b =  @animals_b = 0
+    @latest_average_daily_gain_b = @sum_latest_weights_b = @average_latest_weight_b = 0
+    @sum_latest_daily_gains_o =  @animals_with_gain_o =  @animals_o = 0
+    @latest_average_daily_gain_o = @sum_latest_weights_o = @average_latest_weight_o = 0
+    @latest_weights.each do |animal|
+      animal.daily_gain ||= 0
+      if animal.species == 'bovino' then
+        @sum_latest_daily_gains_b += animal.daily_gain
+        @sum_latest_weights_b += animal.last_weight
+        @animals_b += 1
+        if animal.days_between_weights > 0
+          @animals_with_gain_b += 1
+        end
+      end
+      if animal.species == 'ovino' then
+        @sum_latest_daily_gains_o += animal.daily_gain
+        @sum_latest_weights_o += animal.last_weight
+        @animals_o += 1
+        if animal.days_between_weights > 0
+          @animals_with_gain_o += 1
+        end
+      end
+    end
+    # Bovine KPIs
+    if @animals_with_gain_b > 0 then
+      @latest_average_daily_gain_b = @sum_latest_daily_gains_b / @animals_with_gain_b
+    end
+
+    if @animals_b > 0 then
+      @average_latest_weight_b = @sum_latest_weights_b/@animals_b
+    end
+    # Ovine KPIs
+    if @animals_with_gain_o > 0 then
+      @latest_average_daily_gain_o = @sum_latest_daily_gains_o / @animals_with_gain_o
+    end
+
+    if @animals_o > 0 then
+      @average_latest_weight_o = @sum_latest_weights_o/@animals_o
+    end
+
+    #Every animal should have a weight for data to be accurate
+
+    # Calculate the average Max weight:
+    @average_max_weight = 777
+
+    @days_in_ranch = Weight.select("weights.animal_id as animal_id, animals.species as species,
+      animals.ranch as ranch,  date(NOW()) - MIN(weights.date) as days_in_ranch  ")
+    .joins("JOIN animals ON animals.id = weights.animal_id").group("weights.animal_id, animals.species, animals.ranch")
+    # Calculates average date dif - Días desde ingreso
+    @sum_days_in_ranch_b = @count_a_in_ranch_b = @avg_days_in_ranch_b = 0
+    @sum_days_in_ranch_o = @count_a_in_ranch_o = @avg_days_in_ranch_o = 0
+
+    @days_in_ranch.each do |animal|
+      if animal.species == 'bovino' then
+        @sum_days_in_ranch_b += animal.days_in_ranch
+        @count_a_in_ranch_b += 1
+      end
+      if animal.species == 'ovino' then
+        @sum_days_in_ranch_o += animal.days_in_ranch
+        @count_a_in_ranch_o += 1
+      end
+    end
+    if @count_a_in_ranch_b > 0 then
+      @avg_days_in_ranch_b = @sum_days_in_ranch_b / @count_a_in_ranch_b
+    end
+    if @count_a_in_ranch_o > 0 then
+      @avg_days_in_ranch_o = @sum_days_in_ranch_o / @count_a_in_ranch_o
+    end
+
+    # I use in the home page now the latest daily gain, since it's more dynamic
+
 
     # For the graph
     @data = Weight.totals_by_year_month
 
     # For the Plot Scores
-    # TODO Filter out the score by plot_type
     @recent_scores = PlotEvaluation.select(
       "plot_evaluations.id,
       plot_evaluations.plot_id,
@@ -81,6 +122,7 @@ class PagesController < ApplicationController
       ROUND(CAST((plot_evaluations.water_score + plot_evaluations.pasture_score + plot_evaluations.fences_score) AS decimal )/3,2) as average ")
       .joins("JOIN plots ON plot_evaluations.plot_id = plots.id")
       .where("(plot_evaluations.plot_id, plot_evaluations.id) IN (SELECT plot_id as pi, max(id) as re FROM plot_evaluations GROUP by plot_id)")
+    # Plot  Calculations for Los Sauces
     #Bovinos
     @sum_av_scores_b = 0
     @recent_scores.each {|p| if p.pt == 'bovino' then @sum_av_scores_b += p.average end}
