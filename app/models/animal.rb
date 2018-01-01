b2e61ccb143beaa8bc3aa6f0af3dbdbbbeb39d85
fpto <fabricio.puerto@outlook.com>
2018-01-01 11:15:18 -0600

    Refactored search into animal model.

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,11 @@
 class Animal < ApplicationRecord
   has_many :weights, dependent: :destroy
+
+  def self.search(search)
+    if search
+      where(animal_number: search)
+    else
+      where("animal_number > 0")
+    end
+  end
 end
