6e967ac19260007f9aba114625a16014bdc2e25f
fpto <fabricio.puerto@outlook.com>
2018-01-11 20:36:43 -0600

    Cleaned plot index controller

--- app/controllers/plots_controller.rb
+++ app/controllers/plots_controller.rb
@@ -11,71 +11,9 @@ class PlotsController < ApplicationController
   def index
     # @plots = Plot.all.order('CAST(number AS int)')
     @plots = Plot.latest_plot_scores
-      .search(params[:search])
-      .order(sort_column + " " + sort_direction)
-      .paginate(:page => params[:page], :per_page => 25)
-
-      # We pull animal información in order to calculate ranch load
-      # They require plots and animals to have evaluations and weights
-    @animals = Animal.days_in_ranch_by_ranch
-
-    # TODO Make distinction of Ovino and Bovino plots
-    @sauces_area = 0
-    @laureles_area = 0
-    # Only consider bovino plots
-    @plots.each{ |plot|
-      plot.area ||= 0
-      if plot.ranch == 'sauces' and plot.plot_type == 'bovino' then
-        @sauces_area += plot.area
-      end
-      if plot.ranch == 'laureles' then
-        @laureles_area += plot.area
-      end
-      }
-    @sauces_animals = 0
-    @laureles_animals = 0
-    @animals.each{ |animal| if animal.ranch == 'sauces' then @sauces_animals += 1 end}
-    @animals.each{ |animal| if animal.ranch == 'laureles' then @laureles_animals += 1 end}
-    @sauces_load = 0
-    @laureles_load = 0
-    if @sauces_area > 0 then
-      @sauces_load =  @sauces_animals / @sauces_area
-    end
-    if @laureles_area > 0 then
-      @laureles_load =  @laureles_animals / @laureles_area
-    end
-
-    # Calculate Score per ranch
-    @sauces_plots_b = @sauces_plots_o = @sauces_plots_o = @laureles_plots =@sum_plot_avg_sauces_b = @sum_plot_avg_sauces_o =  @sum_plot_avg_laureles = 0
-    @plots.each{ |plot|
-      case plot.ranch
-      when 'sauces'
-        if plot.plot_type == 'bovino' then
-        @sauces_plots_b += 1
-        @sum_plot_avg_sauces_b += plot.average
-        else
-          @sauces_plots_o += 1
-          @sum_plot_avg_sauces_o += plot.average
-        end
-      when 'laureles'
-        @laureles_plots += 1
-        @sum_plot_avg_laureles += plot.average
-      end }
-    @score_plots_sauces_b =0
-    @score_plots_sauces_o =0
-    @score_plots_laureles =0
-    if @sauces_plots_b > 0 then
-      @score_plots_sauces_b = @sum_plot_avg_sauces_b / @sauces_plots_b
-    end
-    if @sauces_plots_o > 0 then
-      @score_plots_sauces_o = @sum_plot_avg_sauces_o / @sauces_plots_o
-    end
-    if @laureles_plots > 0 then
-      @score_plots_laureles = @sum_plot_avg_laureles / @laureles_plots
-    end
-
-
-
+                 .search(params[:search])
+                 .order(sort_column + " " + sort_direction)
+                 .paginate(:page => params[:page], :per_page => 25)
   end
 
   # GET /plots/1
