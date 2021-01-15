7462de77cbf82ff300e6b270acf40aacc2491b4f
fpto <fabricio.puerto@outlook.com>
2021-01-15 12:52:49 -0600

    Fixed some credential issue and assets.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 
 module Ranch
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
